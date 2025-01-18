c48c3e284d584f4b59213a3d02bc66b8c70f2ef5
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-01-18 20:34:39 +0700

    bump: 1.0.2-demo

--- package.lua
+++ package.lua
@@ -7,8 +7,8 @@ return {
     major = "1",
 		minor = "0",
 		patch = "2",
-		preRelease = "dev",
-		semver = "1.0.2-dev",
+		preRelease = "demo",
+		semver = "1.0.2-demo",
 		build = "",
   },
   ------ Bot custom props ------
