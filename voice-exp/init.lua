b7781b592142cfd787a0ac192a23144de4a79617
UnschooledGamer <unschooledgamer@gmail.com>
2025-01-29 11:47:28 +0530

    deletes PCMString(from voice-exp/xyz.lua)
    clean some mess from voice-exp/init.lua
    
    synced from https://github.com/UnschooledGamer/LunaStream/commit/b804077eb5f5b6be2a6f833915d08428b372aa7e

--- voice-exp/init.lua
+++ voice-exp/init.lua
@@ -1,5 +1,3 @@
--- From https://github.com/SinisterRectus/Discordia
-local PCMString = require('voice-exp.xyz')
 local Voice = require('../src/voice')
 -- {"voice":{"token":"677ca788b33af9e9","endpoint":"india10010.discord.media:443","sessionId":"112235d73f3fb50faca089d2f70d0a6d"}}
 local VoiceClass = Voice('813815427892641832', "1120309844117815326")
@@ -11,19 +9,3 @@ VoiceClass:voiceCredential(
 )
 
 VoiceClass:connect()
-
-local timer = require('timer')
-
-timer.setTimeout(15000, coroutine.wrap(function ()
-  print("play timeout triggered, playing")
-
-  local file = io.open('sample-1.wav', 'rb')
-  if not file then
-    print("file not found")
-    return
-  end
-  file:seek('set', 44); -- skip the header
-  local pcmSting = PCMString(file:read('*all'))
-  print(file.length)
-  VoiceClass:play(pcmSting)
-end))
