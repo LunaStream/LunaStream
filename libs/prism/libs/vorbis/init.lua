ea85f33b59d5e714b43129a312e11825d321a09e
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-01-30 12:47:46 +0700

    add: full experiemental demuxer lib

--- libs/prism/libs/vorbis/init.lua
+++
@@ -1,3 +0,0 @@
-return {
-  WebmDemuxer = require('./WebmDemuxer')
-}
--- libs/prism/prism-exp/ogg.lua
+++
@@ -1,6 +0,0 @@
-local fs = require('fs')
-local prism_opus = require('opus')
-
-fs.createReadStream('./sample/speech_orig.ogg')
-  :pipe(prism_opus.OggDemuxer:new())
-  :pipe(fs.createWriteStream('./results/speech_orig.demuxed.ogg'))
