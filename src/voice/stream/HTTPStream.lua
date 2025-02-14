0bc31d4745643812a6bb681f3ed6383f09b6f3ad
RainyXeon <xeondev@xeondex.onmicrosoft.com>
2025-02-14 20:45:20 +0700

    improve: memory handling on voice stream
    This commit will improve the memory handling on voice stream using
    gc manually to handling. On the provious version, after the voice end,
    stream data still stuck at some variable and make it heavy all them time (100mb)
    
    So in this commit, I optimized the memory by free up some useless memory after
    process and stuff. The result is memory from 100mb all time to:
    60mb -> 50mb -> 40mb -> 30mb -> 20mb depends on song length and quality

--- src/voice/stream/HTTPStream.lua
+++ src/voice/stream/HTTPStream.lua
@@ -2,42 +2,24 @@ local Readable = require('stream').Readable
 
 local HttpStream = Readable:extend()
 
-local custom_string_meta = {
-  __len = function(self)
-    return #self.d
-  end,
-  __index = function(self, key)
-    return self.d[key]
-  end,
-  __tostring = function(self)
-    return self.d
-  end
-}
-
 function HttpStream:initialize(str, chunk_size)
   Readable.initialize(self)
-  self._str = setmetatable({ d = str }, custom_string_meta)
-  self._len = #self._str
-  self.offset = 0
+  self._str = str
   self.chunk_size = chunk_size or 65536
 end
 
 function HttpStream:_read(n)
   n = self.chunk_size or n
-  local i = self._i or 1
-	if i + n < self._len then
-    self.offset = self.offset + n
-		local data = string.sub(tostring(self._str), i, self.offset)
-		self._i = i + n
+	if n < #self._str then
+		local data = string.sub(tostring(self._str), 1, self.chunk_size)
     self:push(data)
+    self._str = string.sub(tostring(self._str), self.chunk_size + 1)
+    collectgarbage('collect')
 		return
 	end
-  setmetatable(self._str, { __mode = "k" })
-  self._len = 0
-  self.offset = 0
   self:push()
-  collectgarbage()
   self._str = nil
+  collectgarbage('collect')
 end
 
 return HttpStream
